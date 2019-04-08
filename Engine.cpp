bc02f072728743cdb65e975e7727a8aecff68314
Dylan Kennedy <epicslurpee@gmail.com>
2019-04-08 13:38:42 -0500

    Added to Engine

--- Engine.cpp
+++ Engine.cpp
@@ -7,6 +7,31 @@ void Game::init(const char *title, int xpos, int ypos, int width, int height,
            bool fullscreen)
 {
 
+  int flags = 0;
+  if(fullscreen)
+    flags = SDL_WINDOW_FULLSCREEN;
+
+  //Initialize Sdl.
+  if(SDL_Init(SDL_INIT_EVERYTHING)== 0){
+    cout << "SDL initialized.";
+
+    //Create window object.
+    _window = SDL_CreateWindow(title, xpos, ypos,width, height, flags);
+    if(_window)
+      cout << "Game's window created.";
+
+    //Create renderer object.
+    _renderer = SDL_CreateRenderer(_window, -1, 0);
+    if(_renderer)
+      cout << "Games renderer initialized.";
+
+    _isRunning = true;
+
+  }else{
+    cerr << "Failed to initialize game";
+    _isRunning = false;
+  }
+
 }
 
 void Game::update(){
